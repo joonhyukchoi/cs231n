085af202408d15fecfdd8b20c569a014597d5a52
joonhyukchoi <cjh4787@gmail.com>
2024-10-12 23:56:54 +0900

    feat: resolve svm loss naive

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -37,6 +37,9 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1  # note delta = 1
             if margin > 0:
                 loss += margin
+                dW[:,j] += X[i]
+                dW[:,y[i]] -= X[i]
+            
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
